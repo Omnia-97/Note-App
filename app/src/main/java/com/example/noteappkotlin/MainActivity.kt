eff42fc462d6bb1b8e0c7f281990ccb2de89fb73
DELL <omnianabil1920@gmail.com>
2024-08-12 18:18:25 +0300

    implemented update

--- app/src/main/java/com/example/noteappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/noteappkotlin/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.noteappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.noteappkotlin.database.NoteDataBase
+import com.example.noteappkotlin.databinding.ActivityMainBinding
+import com.example.noteappkotlin.repository.NoteRepository
 import com.example.noteappkotlin.viewmodel.NoteViewModel
+import com.example.noteappkotlin.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
       lateinit var noteViewModel : NoteViewModel
+      lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDataBase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application , noteRepository)
+        noteViewModel = ViewModelProvider(
+            this,
+            viewModelProviderFactory
+        ).get(NoteViewModel::class.java)
     }
 }
--- app/src/main/java/com/example/noteappkotlin/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/example/noteappkotlin/fragments/UpdateNoteFragment.kt
@@ -1,13 +1,29 @@
 package com.example.noteappkotlin.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.noteappkotlin.MainActivity
 import com.example.noteappkotlin.R
+import com.example.noteappkotlin.databinding.FragmentUpdateNoteBinding
+import com.example.noteappkotlin.model.Note
+import com.example.noteappkotlin.viewmodel.NoteViewModel
 
-class UpdateNoteFragment : Fragment() {
+class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
+    private var _binding: FragmentUpdateNoteBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var currentNote: Note
+    private val args: UpdateNoteFragmentArgs by navArgs()
 
 
     override fun onCreateView(
@@ -15,7 +31,72 @@ class UpdateNoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_note, container, false)
+        _binding = FragmentUpdateNoteBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        noteViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note!!
+        binding.etNoteTitleUpdate.setText(currentNote.noteTitle)
+        binding.etNoteBodyUpdate.setText(currentNote.noteBody)
+        binding.fabDone.setOnClickListener {
+            val title = binding.etNoteTitleUpdate.text.toString().trim()
+            val body = binding.etNoteBodyUpdate.text.toString().trim()
+            if (title.isNotEmpty()) {
+                val note = Note(currentNote.id, title, body)
+                noteViewModel.updateNote(note)
+                view.findNavController().navigate(R.id.action_updateNoteFragment_to_homeFragment2)
+
+            }
+            else{
+                Toast.makeText(context, "Please Enter note title", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+    private fun deleteNote(){
+        AlertDialog.Builder(activity).apply {
+            setTitle("Delete Note")
+            setMessage("Are you sure to delete this note?")
+            setPositiveButton("Yes"){dialog, which ->
+                noteViewModel.deleteNote(currentNote)
+                view?.findNavController()?.navigate(R.id.action_updateNoteFragment_to_homeFragment2)
+            }
+            setNegativeButton("Cancel" , null).
+            create() .
+            show()
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_update_note, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_save -> {
+                deleteNote()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
